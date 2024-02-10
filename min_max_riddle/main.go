a42b5d4ec9e503592ae277c4a07d9d55334b16be
ibrahimkhidr <akhidrb@gmail.com>
2024-02-10 05:20:43 +0200

    go leetcode

--- min_max_riddle/main.go
+++ min_max_riddle/main.go
@@ -10,33 +10,61 @@ import (
 )
 
 func riddle(arr []int64) []int64 {
-	n := len(arr)
-	lowList := make([][]int64, n)
-	for i := 0; i < n; i++ {
-		lowList[i] = make([]int64, n)
-	}
-	for i := 0; i < n; i++ {
-		least := arr[i]
-		k := 0
-		for j := i; j < n; j++ {
-			if arr[j] <= least {
-				least = arr[j]
+	wMax := make(map[int64]int64)
+	stk := make([][]int64, 0)
+	var i int64
+	n := int64(len(arr))
+	arr = append(arr, 0)
+	for i = 0; i < int64(len(arr)); i++ {
+		idx := i
+		for len(stk) > 0 && stk[len(stk)-1][0] > arr[i] {
+			val, prevIdx := stk[len(stk)-1][0], stk[len(stk)-1][1]
+			stk = stk[:len(stk)-1]
+			temp1 := i - prevIdx + 1
+			if w, ok := wMax[arr[i]]; ok {
+				if w > temp1 {
+					temp1 = w
+				}
 			}
-			lowList[k][i] = least
-			k++
+			wMax[arr[i]] = temp1
+
+			temp2 := i - prevIdx
+			if w, ok := wMax[val]; ok {
+				if w > temp2 {
+					temp2 = w
+				}
+			}
+			wMax[val] = temp2
+			idx = prevIdx
 		}
+		stk = append(stk, [][]int64{{arr[i], idx}}...)
 	}
-	highList := make([]int64, n)
-	for i := 0; i < n; i++ {
-		highest := lowList[i][0]
-		for j := 0; j < n-i; j++ {
-			if lowList[i][j] > highest {
-				highest = lowList[i][j]
+	delete(wMax, 0)
+	wInv := make(map[int64]int64)
+	for key, value := range wMax {
+		if w, ok := wInv[value]; ok {
+			if w > key {
+				key = w
 			}
 		}
-		highList[i] = highest
+		wInv[value] = key
 	}
-	return highList
+	ll := make([]int64, n)
+	ll[0] = wInv[n]
+	for i = n - 1; i >= 1; i-- {
+		temp := ll[n-i-1]
+		if w, ok := wInv[i]; ok {
+			if w > temp {
+				temp = w
+			}
+		}
+		ll[n-i] = temp
+	}
+	var j int64
+	for i, j = 0, n-1; i < j; i, j = i+1, j-1 {
+		ll[i], ll[j] = ll[j], ll[i]
+	}
+	return ll
 }
 
 func main() {
