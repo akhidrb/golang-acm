abc9ad3f4eb2843cee9d841d28349a407563f4bd
ibrahimkhidr <akhidrb@gmail.com>
2026-01-16 22:04:18 -0800

    post order traversal minimum fuel cost

--- post_order_traversal/main.go
+++ post_order_traversal/main.go
@@ -1,22 +1,5 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-func postorderTraversal(root *TreeNode) []int {
-	output := make([]int, 0)
-	pot(root, &output)
-	return output
-}
-
-func pot(root *TreeNode, output *[]int) {
-	if root == nil {
-		return
-	}
-	pot(root.Left, output)
-	pot(root.Right, output)
-	*output = append(*output, root.Val)
+func minimumFuelCost(roads [][]int, seats int) int64 {
+	return 0
 }
--- post_order_traversal/main_test.go
+++ post_order_traversal/main_test.go
@@ -8,48 +8,9 @@ import (
 func Test(t *testing.T) {
 
 	t.Run("1", func(t *testing.T) {
-		p := []any{1, nil, 2, 3}
-		pTree := buildTree(p)
-		output := postorderTraversal(pTree)
-		assert.Equal(t, []int{3, 2, 1}, output)
+		roads := [][]int{{0, 1}, {0, 2}, {0, 3}}
+		output := minimumFuelCost(roads, 5)
+		assert.Equal(t, int64(3), output)
 	})
 
-	t.Run("2", func(t *testing.T) {
-		p := []any{1, 2, 3, 4, 5, nil, 8, nil, nil, 6, 7, 9}
-		pTree := buildTree(p)
-		output := postorderTraversal(pTree)
-		assert.Equal(t, []int{4, 6, 7, 5, 2, 9, 8, 3, 1}, output)
-	})
-
-}
-
-func buildTree(level []any) *TreeNode {
-	if len(level) == 0 || level[0] == nil {
-		return nil
-	}
-
-	root := &TreeNode{Val: level[0].(int)}
-	q := []*TreeNode{root}
-	i := 1
-
-	for len(q) > 0 && i < len(level) {
-		node := q[0]
-		q = q[1:]
-
-		// left child
-		if i < len(level) && level[i] != nil {
-			node.Left = &TreeNode{Val: level[i].(int)}
-			q = append(q, node.Left)
-		}
-		i++
-
-		// right child
-		if i < len(level) && level[i] != nil {
-			node.Right = &TreeNode{Val: level[i].(int)}
-			q = append(q, node.Right)
-		}
-		i++
-	}
-
-	return root
 }
