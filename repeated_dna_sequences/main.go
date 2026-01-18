10e7d3a977e40221775485f13dd6547b19c86679
ibrahimkhidr <akhidrb@gmail.com>
2026-01-17 18:44:47 -0800

    repeated dna sequence

--- repeated_dna_sequences/main.go
+++ repeated_dna_sequences/main.go
@@ -1,35 +1,16 @@
 package main
 
 func findRepeatedDnaSequences(s string) []string {
-	if len(s) < 10 {
-		return nil
-	}
-
-	// Map DNA char -> 2-bit code
-	code := [256]int{
-		'A': 0,
-		'C': 1,
-		'G': 2,
-		'T': 3,
-	}
-
-	const L = 10
-	const mask = (1 << (2 * L)) - 1
-
-	seen := make(map[int]int, len(s))
+	countMap := make(map[string]int)
 	res := make([]string, 0)
-
-	x := 0
-	for i := 0; i < len(s); i++ {
-		x = ((x << 2) | code[s[i]]) & mask
-
-		if i >= L-1 {
-			seen[x]++
-			if seen[x] == 2 {
-				res = append(res, s[i-L+1:i+1])
-			}
+	start, end := 0, 9
+	for end < len(s) {
+		countMap[s[start:end+1]]++
+		if countMap[s[start:end+1]] == 2 {
+			res = append(res, s[start:end+1])
 		}
+		start++
+		end++
 	}
-
 	return res
 }
