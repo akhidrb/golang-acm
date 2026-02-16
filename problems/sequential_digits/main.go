60e2b3c8eb242b45517688e7b8608f682511f8fa
ibrahimkhidr <akhidrb@gmail.com>
2026-02-15 19:20:13 -0800

    1291. Sequential Digits

--- problems/sequential_digits/main.go
+++ problems/sequential_digits/main.go
@@ -1,51 +1,18 @@
 package main
 
-import "strconv"
+import "sort"
 
 func sequentialDigits(low int, high int) []int {
-	s := strconv.Itoa(low)
-	first, _ := strconv.Atoi(string(s[0]))
 	nums := make([]int, 0)
-	n := len(s)
-	if n-1+first > 9 {
-		first = 1
-		n++
-	}
-	for i := 1; i < n; i++ {
-		mod := (first % 10) + 1
-		first *= 10
-		first += mod
-	}
-	str := strconv.Itoa(first)
-	lastNum, _ := strconv.Atoi(string(str[len(str)-1]))
-	for first <= high && len(str) > 1 {
-		if first >= low {
-			nums = append(nums, first)
-		}
-		if lastNum == 9 {
-			first = 1
-			for i := 1; i < len(str)+1; i++ {
-				mod := (first % 10) + 1
-				first *= 10
-				first += mod
-			}
-			str = strconv.Itoa(first)
-			if first <= high {
-				nums = append(nums, first)
+	for i := 1; i <= 9; i++ {
+		num := i
+		for j := i + 1; j <= 9; j++ {
+			num = (num * 10) + j
+			if num >= low && num <= high {
+				nums = append(nums, num)
 			}
 		}
-		if lastNum == 9 && len(str) == 9 {
-			break
-		}
-		str = str[1:]
-		last := str[len(str)-1]
-		first, _ = strconv.Atoi(str)
-		lastNum, _ = strconv.Atoi(string(last))
-		first *= 10
-		lastNum++
-		first += lastNum
-		str = strconv.Itoa(first)
 	}
-
+	sort.Ints(nums)
 	return nums
 }
