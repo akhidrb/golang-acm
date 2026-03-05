d24d5b1834aad300fa5e11a506c5b21f66fbea46
ibrahimkhidr <akhidrb@gmail.com>
2026-03-05 15:06:22 -0800

    316. Remove Duplicate Letters

--- problems/remove_duplicate_letters/main.go
+++ problems/remove_duplicate_letters/main.go
@@ -1,6 +1,29 @@
 package main
 
 func removeDuplicateLetters(s string) string {
+	seen := make(map[rune]bool)
+	counter := make(map[rune]int)
+	stack := make([]rune, 0, 26)
+
+	for _, ch := range s {
+		counter[ch]++
+	}
+
+	for _, ch := range s {
+		counter[ch]--
+		if !seen[ch] {
+			for len(stack) > 0 && ch < stack[len(stack)-1] && counter[stack[len(stack)-1]] > 0 {
+				delete(seen, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			seen[ch] = true
+			stack = append(stack, ch)
+		}
+	}
+	return string(stack)
+}
+
+func removeDuplicateLetters2(s string) string {
 	seen := make(map[rune]int)
 	lastOcc := make(map[rune]int)
 	stack := make([]rune, 0, 26)
