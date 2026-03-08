9b0d257aa72fb97b8ce51057e71541bb3996dc5f
ibrahimkhidr <akhidrb@gmail.com>
2026-03-08 16:10:52 -0700

    insertion sort

---
+++ coding/sorting/insertion/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test(t *testing.T) {
+
+	t.Run("1", func(t *testing.T) {
+		nums := []int{10, 9, 2, 5, 3, 7, 101, 18}
+		insertionSort(nums)
+		assert.Equal(t, []int{2, 3, 5, 7, 9, 10, 18, 101}, nums)
+	})
+
+}
