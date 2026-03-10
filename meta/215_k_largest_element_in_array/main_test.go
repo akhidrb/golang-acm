74fdc3870678c87cdf7443a9ff7ad0ab142a69b6
ibrahimkhidr <akhidrb@gmail.com>
2026-03-10 01:29:13 -0700

    215. Kth Largest Element in an Array

--- meta/215_k_largest_element_in_array/main_test.go
+++ meta/215_k_largest_element_in_array/main_test.go
@@ -13,4 +13,16 @@ func Test(t *testing.T) {
 		assert.Equal(t, 5, valid)
 	})
 
+	t.Run("2", func(t *testing.T) {
+		nums := []int{3, 2, 1, 5, 6, 4}
+		valid := findKthLargestSorting(nums, 2)
+		assert.Equal(t, 5, valid)
+	})
+
+	t.Run("3", func(t *testing.T) {
+		nums := []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
+		valid := findKthLargestSorting(nums, 4)
+		assert.Equal(t, 4, valid)
+	})
+
 }
---
+++ meta/215_k_largest_element_in_array/sorting.go
@@ -0,0 +1,33 @@
+package main
+
+func findKthLargestSorting(nums []int, k int) int {
+	nums = mergeSort(nums)
+	return nums[len(nums)-k]
+}
+
+func mergeSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+	mid := len(nums) / 2
+	left := mergeSort(nums[:mid])
+	right := mergeSort(nums[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	i, j := 0, 0
+	result := make([]int, 0, len(left)+len(right)+1)
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}
